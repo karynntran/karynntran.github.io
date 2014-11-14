a1b429abdbe2d01286e934f66ad78493d675b3e9
karynntran <karynn.tran@gmail.com>
2014-11-13 23:59:06 -0500

    Work on Jquery to swap out content divs, not working yet

--- scripts.js
+++ scripts.js
@@ -1,14 +1,34 @@
 console.log(':)')
 
 // $(document).ready(function(){
+//   $('#about-me-link').on('click', function(){
+//     $('#about-me').show();
+//     $('#experience').hide();
+//     $('#coding-projects').hide();
+//     $('#links').hide();
+//  	});
 
-// 	$('#about-me-link').click(function(){
-// 	  $('.about-me').show();
-// 	});
 
-// 	$('#experience-link').click(function(){ 
-// 	  $('.box').empty();
-// 	  $('.experience').show();
-// 	});
+//   $('#experience').on('click', function(){
+//     $('#about-me').css("display","none");
+//     $('#experience').show();
+//     $('#coding-projects').css("display","none");
+//     $('#links').css("display","none");
+//  	});
+
+
+//  	$('#coding-projects').on('click', function(){
+//     $('#about-me').hide();
+//     $('#experience').hide();
+//     $('#coding-projects').show();
+//     $('#links').hide();
+//  	});
+
+//  	$('#links').on('click', function(){
+//     $('#about-me').hide();
+//     $('#experience').hide();
+//     $('#coding-projects').hide();
+//     $('#links').show();
+// //  	});
 
 // });
