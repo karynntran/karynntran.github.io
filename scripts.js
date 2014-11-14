12dceb00f1aea9760eea62773f38118e641c51b6
karynntran <karynn.tran@gmail.com>
2014-11-13 23:25:33 -0500

    Update github page

---
+++ scripts.js
@@ -0,0 +1,14 @@
+console.log(':)')
+
+// $(document).ready(function(){
+
+// 	$('#about-me-link').click(function(){
+// 	  $('.about-me').show();
+// 	});
+
+// 	$('#experience-link').click(function(){ 
+// 	  $('.box').empty();
+// 	  $('.experience').show();
+// 	});
+
+// });
